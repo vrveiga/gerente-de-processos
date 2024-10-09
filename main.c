a2be484f4aa7b8c8dd6e6a30d446f26d5fcd135c
vrveiga <141955738+vrveiga@users.noreply.github.com>
2024-10-09 08:40:59 -0300

    Implementar vetor tempo

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdbool.h>
 
 #define MAX_DESCR 55
 
@@ -20,7 +21,7 @@ int busca_binaria_prior(celula* proc, int sz, int p) {
     int ret = 0;
 
     while (l <= r) {
-        int m = (l + r) / 2;
+        int m = (l + r)/2;
         if (proc[m].prior < p) {
             ret = m;
             r = m - 1;
@@ -32,6 +33,32 @@ int busca_binaria_prior(celula* proc, int sz, int p) {
     return l;
 }
 
+bool eh_menor(horario a, horario b) {
+    if (a.hh < b.hh) return true;
+    if (a.hh > b.hh) return false;
+    if (a.mm < b.mm) return true;
+    if (a.mm > b.mm) return false;
+    if (a.ss < b.ss) return true;
+    if (a.ss > b.ss) return false;
+}
+
+int busca_binaria_tempo(celula* proc, int sz, horario t) {
+    int l = 0, r = sz - 1;
+    int ret = sz;
+
+    while (l <= r) {
+        int m = (l + r)/2;
+        if (eh_menor(t, proc[m].chegada)) {
+            ret = m;
+            r = m - 1;
+        } else {
+            l = m + 1;
+        }
+    }
+
+    return ret;
+}
+
 void add(celula* p_prior, celula* p_tempo, int* sz) {
     int prior, hh, mm, ss;
     char desc[MAX_DESCR];
@@ -40,13 +67,21 @@ void add(celula* p_prior, celula* p_tempo, int* sz) {
     horario tempo = {hh, mm, ss};
 
     int id = busca_binaria_prior(p_prior, *sz, prior);
-
     for (int i = *sz; i > id; i--)
         p_prior[i] = p_prior[i-1];
 
     p_prior[id].prior = prior;
     p_prior[id].chegada = tempo;
     strncpy(p_prior[id].descricao, desc, MAX_DESCR);
+
+    id = busca_binaria_tempo(p_tempo, *sz, tempo);
+    printf("%d\n", id);
+    for (int i = *sz; i > id; i--)
+        p_tempo[i] = p_tempo[i-1];
+
+    p_tempo[id].prior = prior;
+    p_tempo[id].chegada = tempo;
+    strncpy(p_tempo[id].descricao, desc, MAX_DESCR);
 
     (*sz)++;
 }
@@ -60,7 +95,13 @@ void print(celula* p_prior, celula* p_tempo, int sz) {
             printf("%d %02d:%02d:%02d %s\n", p_prior[i].prior, p_prior[i].chegada.hh,
                     p_prior[i].chegada.mm, p_prior[i].chegada.ss, p_prior[i].descricao);
         }
+    } else if (op == 't') {
+        for (int i = 0; i < sz; i++) {
+            printf("%d %02d:%02d:%02d %s\n", p_tempo[i].prior, p_tempo[i].chegada.hh,
+                    p_tempo[i].chegada.mm, p_tempo[i].chegada.ss, p_tempo[i].descricao);
+        }
     }
+    printf("\n");
 }
 
 int main() {
@@ -77,4 +118,6 @@ int main() {
 
         
     }
+
+    return 0;
 }
